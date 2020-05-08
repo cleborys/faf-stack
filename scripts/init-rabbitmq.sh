dec812ee4a9237c7ba65c84a03ea6b3597b8ad66
Clemens Borys <clemens@borys.dk>
2020-05-08 22:43:22 +0200

    Add RabbitMQ user faf-lobby and make faf-rabbitMQ accessible on localhost:5672

--- scripts/init-rabbitmq.sh
+++ scripts/init-rabbitmq.sh
@@ -13,6 +13,11 @@ docker-compose up -d faf-rabbitmq
 
 # Create RabbitMQ users
 docker-compose exec faf-rabbitmq rabbitmqctl wait --timeout ${MAX_WAIT} "${RABBITMQ_PID_FILE}"
+
 docker-compose exec faf-rabbitmq rabbitmqctl add_vhost "${RABBITMQ_POSTAL_VHOST}"
 docker-compose exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_POSTAL_USER}" "${RABBITMQ_POSTAL_PASS}"
 docker-compose exec faf-rabbitmq rabbitmqctl set_permissions -p "${RABBITMQ_POSTAL_VHOST}" "${RABBITMQ_POSTAL_USER}" ".*" ".*" ".*"
+
+docker-compose exec faf-rabbitmq rabbitmqctl add_vhost "${RABBITMQ_LOBBYSERVER_VHOST}"
+docker-compose exec faf-rabbitmq rabbitmqctl add_user "${RABBITMQ_LOBBYSERVER_USER}" "${RABBITMQ_LOBBYSERVER_PASS}"
+docker-compose exec faf-rabbitmq rabbitmqctl set_permissions -p "${RABBITMQ_LOBBYSERVER_VHOST}" "${RABBITMQ_LOBBYSERVER_USER}" ".*" ".*" ".*"
